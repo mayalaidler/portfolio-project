e64f40a399699d573eaf90d1fec6506280745136
mayalaidler <mayalaidler@gmail.com>
2026-07-29 08:03:20 -0400

    Fix run_test.sh venv path, remove editor config and unused run.py, clarify desk hint

--- run.py
+++
@@ -1,5 +0,0 @@
-import os
-from app import app
-
-port = int(os.environ.get("PORT", 5001))
-app.run(host="0.0.0.0", port=port)
--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-$PWD/python-virtualenv/bin/python -m unittest discover -v tests/
+if [ -x "$PWD/python3-virtualenv/bin/python" ]; then
+    PYTHON="$PWD/python3-virtualenv/bin/python"
+elif [ -x "$HOME/python3-virtualenv/bin/python" ]; then
+    PYTHON="$HOME/python3-virtualenv/bin/python"
+else
+    PYTHON="python3"
+fi
 
+"$PYTHON" -m unittest discover -v tests/
